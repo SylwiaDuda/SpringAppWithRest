c154762b5fb77550ec9350a1121bab6961259168
Sylwia Duda <sylwia.agnieszka.duda@gmail.com>
2019-05-28 15:28:50 +0200

    Initial commit

---
+++ src/main/java/com/pl/springappwithrest/SpringappwithrestApplication.java
@@ -0,0 +1,13 @@
+package com.pl.springappwithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappwithrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringappwithrestApplication.class, args);
+    }
+
+}
